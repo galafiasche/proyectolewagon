bab9e388181cfa016ccec3ed7b2aa022b42e2aad
galafiasche <59350076+galafiasche@users.noreply.github.com>
2020-08-06 02:24:56 -0300

    Create javaa.js

---
+++ javaa.js
@@ -0,0 +1,9 @@
+function validateText() {
+  var name = document.forms.Quiz.fname.value;
+  if (name == "") {
+    alert("Gracias por suscribirte 🐾🐾");
+    return false;
+  } else {
+    return true;
+  }  
+}
